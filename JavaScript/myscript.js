6ede4477ae2e2c03610e08ff5a9f2cca7f236f2c
dkdrk <dkdrk452@gmail.com>
2016-01-22 15:04:24 +0530

    add js

---
+++ JavaScript/myscript.js
@@ -0,0 +1,9 @@
+      var foo = true, baz = 10;
+      var bar=12;
+      if (foo) {
+        bar = 3;
+      }
+      if (baz > bar) {
+        document.write( baz + "<br>");
+      }
+      document.write(bar);
